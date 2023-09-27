865bbc35f7a88a02671180a8ec9957a8a7c76b69
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2023-09-27 08:03:22 -0300

    Criação da pasta venv e criação das primeiras rota

--- venv/index.py
+++ venv/index.py
@@ -1,9 +1,16 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/aularodrig')
 def index():
     return "OLa"
+
+
+
+@app.route('/outrarora')
+def outra():
+    return "<h1>Outra Rota</h1>"
 
 app.run()
