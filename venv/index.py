87940471697331ebd2e0996c73ec63a08dafae0f
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2023-10-03 16:59:48 -0300

    criação de páginas e rotas com pythom e html

--- venv/index.py
+++ venv/index.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('initPage.html')
+    return render_template('initPage.html', titulo = 'INIT PAGE')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', titulo = 'LOGIN')
 
 @app.route('/login/cliente')
 def cliente():
-    return render_template('cliente.html')
+    return render_template('cliente.html', titulo = 'PAGE CLIENT')
 
 app.run()
