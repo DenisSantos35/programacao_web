b76d59e7d6d72e364f502544332fb33c4e47e057
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2023-10-04 17:46:06 -0300

    criacao de rotas apos inserir senha de login

--- venv/index.py
+++ venv/index.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('initPage.html', titulo = 'INIT PAGE')
-
-@app.route('/login')
-def login():
     return render_template('login.html', titulo = 'LOGIN')
+
+@app.route('/login', methods=['POST'])
+def login():
+    login_user = request.form['login']
+    senha = request.form['senha']
+    if login_user == 'admin' and senha == '123':
+        return render_template('cliente.html', titulo = 'PAGE CLIENT')
+    else:
+        return render_template('login.html', titulo = 'LOGIN', resposta = 'Usuario ou senha')
+    
 
 @app.route('/login/cliente')
 def cliente():
-    return render_template('cliente.html', titulo = 'PAGE CLIENT')
+    return 
 
 app.run()
