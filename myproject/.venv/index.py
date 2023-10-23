f9cafe116d58613e9eb1ca37adb49ae0b6106e8d
Denis Diogo dos Santos <111183537+DenisSantos35@users.noreply.github.com>
2023-10-23 11:45:32 -0300

    conecção com servidor do banco mysql

--- myproject/.venv/index.py
+++ myproject/.venv/index.py
@@ -1,22 +1,11 @@
 from flask import Flask, render_template, request
-#from mysql.connector import connect
+import mysql.connector
 
 
 
 
-app = Flask(__name__)
 
-'''def mysql_connection():
-    connection = connect(
-        host = 'localhost',
-        user = 'root',
-        password = '1234',
-        database = 'nodemysql2'
-    )    
-    connection =  mysql_connection()
-    query = "INSERT INTO books (title, pageqty) VALUES (%s,%s)"
-    cursor = connection.cursor()
-    cursor.execute(query)'''
+app = Flask(__name__)    
 
 
 @app.route("/")
@@ -35,18 +24,23 @@ def initPage():
         return render_template('homePage.html', Titulo = "Home Page")
     else:
         return render_template('loginPage.html', Titulo = "Página Login", resposta = "Usuário ou senha incorretos") 
+@app.route("/homePage")
+def getHomePage():
+    return render_template('homePage.html', Titulo = "Home Page")
     
 @app.route("/createUser")
 def createUser():
     return render_template('createAcount.html', titulo = "CADASTRAR USUÁRIO") 
 
 @app.route("/createUser/insert", methods = ["POST"])
-def  saveDataBase():
+def saveDataBase():
     name = request.form['name']
-    senha = request.form['senha']
-    print(name, senha)
+    email =request.form['email']
+    senha = request.form['senha']    
+    
+    newUserInsert(name, senha, email)      
     return render_template('homePage.html', Titulo = "Home Page", resposta = 'Deseja cadastrar cliente') 
-
+    
 @app.route("/createUser/cliente")
 def createCliente():
     return render_template('cadastroCliente.html', titulo = 'Cadastrar Cliente', resposta = 'Cliente cadastrado com sucesso')
@@ -63,10 +57,30 @@ def searchCliente():
     
     print(name, cpf, email, endereco, bairro, cep, cidade)
     return render_template('homePage.html', Titulo = "Home Page", resposta ='Cliente cadastrado com sucesso' )
-    
-    
 
+
+# Conecao com banco e um insert
+def conn():
+    return mysql.connector.connect(
+        host = 'mysql01.cgkdrobnydiy.us-east-1.rds.amazonaws.com',
+        user = 'aluno_fatec',
+        password = 'aluno_fatec',   
+        database = 'meu_banco' 
+    )
     
+# insert no banco de dados
+def newUserInsert(user, password, email):
+    db = conn()
+    myCursor = db.cursor()
+    query = "INSERT INTO denis_TB_user (USUARIO, SENHA, EMAIL) VALUES (%s,%s, %s)"
+    values = (user, password, email)
+    myCursor.execute(query, values)
+    db.commit()
+    return "Dados salvo com sucesso"
+
+
+
+   
 
 app.run()
     
